8e4d1963306a3df8cea69c2498197a50694478b4
Joshua Knauber <joshua.knauber@gmail.com>
2025-03-02 02:18:28 +0100

    readme

--- examples/simple-react-ts/src/App.tsx
+++ examples/simple-react-ts/src/App.tsx
@@ -3,9 +3,7 @@ import { intl } from "./lib/intl";
 function App() {
   return (
     <>
-      <span>
-        testing this {intl.get("nested.nested-placeholders", { amount: 123 })}
-      </span>
+      <span>testing this {intl.get("other", { test: 123 })}</span>
     </>
   );
 }
